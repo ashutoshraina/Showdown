2d90b4b5514700dd965f277f7c613a130b5cb93c
Ashutosh Raina <ashutoshraina1989@gmail.com>
2013-05-26 00:27:53 +0530

    Added extension methods that will be useful for model binding.
    Added the recipes controller.
    Added the corresponding view.
    Bare bones structure only.
    Will be changed in the future.

---
+++ Showdown.Nancy/Helpers/EnumerableExtensionForEach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Showdown.Nancy.Helpers
+    {
+    public static class EnumerableExtensionForEach
+        {
+        public static void ForEach<T>( this IEnumerable<T> list, Action<T> block )
+            {
+            foreach (var item in list)
+                {
+                    block( item );
+                }
+            }
+        }
+    }
---
+++ Showdown.Nancy/Modules/RecipeModule.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Nancy;
+using Showdown.Model;
+
+namespace Showdown.Nancy.Modules
+{
+    public class RecipeModule : NancyModule
+    {
+        public RecipeModule() : base("/recipe")
+        {
+            Get["/list"] = parameter =>
+                           {
+                             return View["/recipes/Index",GetRecipes()];
+                           };
+            Post["/create"] = parameter =>
+                            {
+                            return null;
+                            };
+        }
+
+        private List<Recipe> GetRecipes()
+        {
+        return new List<Recipe>
+                   {
+                       new Recipe{
+                                    Id = 1 , 
+                                    Name = "Spinach Awesomeness" , 
+                                    Category = new List<string>{"Veg, Non-Veg, Vegan"},
+                                    Content = " I have no idea what the content should be, let puri figure it out.",
+                                    Ingredients = new List<Ingredient>{new Ingredient{Name="Spinach", Quantity="500gm"}},
+                                    RecipeImageUrl = "I don't have anything right now"
+                                  },
+                        new Recipe{
+                                    Id = 2 , 
+                                    Name = "Potato Awesomeness" , 
+                                    Category = new List<string>{"Veg, Non-Veg, Vegan"},
+                                    Content = " I have no idea what the content should be, let puri figure it out.",
+                                    Ingredients = new List<Ingredient>{new Ingredient{Name="Potato", Quantity="500gm"}},
+                                    RecipeImageUrl = "I don't have anything right now"
+                                  }
+                   };
+        }
+    }
+}
