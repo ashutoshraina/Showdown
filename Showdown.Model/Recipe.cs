40a9b0e20103fe2eccc5cda040be63698007e764
Ashutosh Raina <ashutoshraina1989@gmail.com>
2013-06-16 21:42:13 +0530

    Recipe view is now editable.
    Created a category enum for recipes, switched from tables to div layout
    in Recipe/Index.

--- Showdown.Model/Recipe.cs
+++ Showdown.Model/Recipe.cs
@@ -2,13 +2,22 @@
 
 namespace Showdown.Model
 {
+    public enum Category
+    {
+        Veg,
+        Cakes,
+        Pastry,
+        Vegan,
+        NonVeg
+    }
+
     public class Recipe
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public IEnumerable<string> Category { get; set; }
+        public Category Category { get; set; }
 
         public string Content { get; set; }
 
--- Showdown.Nancy/Modules/RecipeModule.cs
+++ Showdown.Nancy/Modules/RecipeModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Nancy;
 using Showdown.Model;
 
@@ -10,7 +11,14 @@ namespace Showdown.Nancy.Modules
         {
             Get["/list"] = parameter =>
                            {
-                             return View["/recipes/Index",GetRecipes()];
+                               var temp = GetRecipes();
+                               var result = from r in temp
+                                            group r by r.Category
+                                            into groupedByCategory
+                                            orderby groupedByCategory.Key
+                                            select groupedByCategory;
+
+                             return View["/recipes/Index",result];
                            };
             Get["/create"] = parameter =>
                             {
@@ -19,6 +27,18 @@ namespace Showdown.Nancy.Modules
             Post["/create"] = parameter =>
                             {
                             return null;
+                            };
+            Put["/name"] = parameter =>
+                            {
+                                return null;
+                            };
+            Put["/category"] = parameter =>
+                            {
+                                return null;
+                            };
+            Put["/content"] = parameter =>
+                            {
+                                return null;
                             };
         }
 
@@ -29,19 +49,27 @@ namespace Showdown.Nancy.Modules
                        new Recipe{
                                     Id = 1 , 
                                     Name = "Spinach Awesomeness" , 
-                                    Category = new List<string>{"Veg, Non-Veg, Vegan"},
+                                    Category = Category.Veg,
                                     Content = " I have no idea what the content should be, let puri figure it out.",
-                                    Ingredients = new List<Ingredient>{new Ingredient{Name="Spinach", Quantity="500gm"}},
+                                    Ingredients = new List<Ingredient>{new Ingredient{Name="Spinach", Quantity="500gm"}, new Ingredient{Name = "Flour" , Quantity = "250gm"}},
                                     RecipeImageUrl = "I don't have anything right now"
                                   },
                         new Recipe{
                                     Id = 2 , 
                                     Name = "Potato Awesomeness" , 
-                                    Category = new List<string>{"Veg, Non-Veg, Vegan"},
+                                    Category = Category.Cakes,
                                     Content = " I have no idea what the content should be, let puri figure it out.",
                                     Ingredients = new List<Ingredient>{new Ingredient{Name="Potato", Quantity="500gm"}},
                                     RecipeImageUrl = "I don't have anything right now"
-                                  }
+                                  },
+                        new Recipe{
+                        Id = 3 , 
+                        Name = "Potato Awesomeness" , 
+                        Category = Category.Cakes,
+                        Content = " I have no idea what the content should be, let puri figure it out.",
+                        Ingredients = new List<Ingredient>{new Ingredient{Name="Potato", Quantity="500gm"}},
+                        RecipeImageUrl = "I don't have anything right now"
+                        }
                    };
         }
     }
